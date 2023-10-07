007c2d08c1bfa0a9df12ef2d7dd79f28fc5b9804
DianaBerte <dianaberte.go@gmail.com>
2023-10-07 19:33:34 +0200

    Update App.js

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -3,14 +3,16 @@ import 'tailwindcss/tailwind.css';
 
 export default function App() {
   return (
-    <div className="bg-black">
-      <div className='bg-red-500'>
-        <h5 className='text-3xl text-yellow-100 font-bold underline'>Hey</h5>
+    <div className="h-screen bg-cyan-900">
+      <div className="bg-black">
+        <div className='bg-pink-400'>
+          <h5 className='text-3xl text-yellow-100 font-bold underline'>Hey</h5>
+        </div>
+        <p className='text-red-200'>hello</p>
+        <h1 className="text-3xl text-cyan-500 font-bold underline">
+          bye
+        </h1>
       </div>
-      <p className='text-red-200'>hello</p>
-      <h1 className="text-3xl text-cyan-500 font-bold underline">
-        bye
-      </h1>
     </div>
   )
 }
