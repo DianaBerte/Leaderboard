aebc4391eb18951579579535aa68591c851ee728
DianaBerte <dianaberte.go@gmail.com>
2023-10-12 16:19:32 +0200

    TBD: #17

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
               </h1>
             </div>
             <ul className='mt-6'>
-              <li className='py-1 flex justify-end'> {/* flex justify-end to align the content to the right */}
+              <li className='py-1 flex justify-end'> {/* flex justify-end: content to the right */}
                 <button className='hover:text-lg hover:text-white px-4 flex justify-end'>
                   <span>Home</span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-1"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
