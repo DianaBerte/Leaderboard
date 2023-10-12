e97a96e685328754a24b81d2e09dff73bf8f92e6
DianaBerte <dianaberte.go@gmail.com>
2023-10-12 15:57:20 +0200

    Update App.jsx

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -10,21 +10,21 @@ export default function App() {
           <nav>
             <div>
               <h1 className='text-2xl font-extrabold uppercase p-4 border-b border-cyan-700'>
-                <a href="/">Leaderboard</a>
+                <a className='hover:text-white' href="/">Leaderboard</a>
               </h1>
             </div>
             <ul>
               <li>
-                <button>
+                <button className='hover:text-lg hover:text-white'>
                   <span>Home</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg></button>
               </li>
               <li>
-                <button>
+                <button className='hover:text-lg hover:text-white'>
                   <span>About</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" /></svg>
                 </button>
               </li>
               <li>
-                <button>
+                <button className='hover:text-lg hover:text-white'>
                   <span>Contact</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
                 </button>
               </li>
@@ -34,8 +34,8 @@ export default function App() {
 
         <div className='px-16 py-6 md:col-span-2'>{/* main content wrapper */}
           <div className='flex justify-center md:justify-end'>
-            <button className='rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider border-primary md:border-2'>Log in</button>
-            <button className='rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider border-primary md:border-2 ml-2'>Sign up</button>
+            <button className='rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider border-primary md:border-2 hover:bg-fourth hover:text-cyan-900'>Log in</button>
+            <button className='rounded-full py-2 px-3 uppercase text-xs font-bold cursor-pointer tracking-wider border-primary md:border-2 ml-2 hover:bg-fourth hover:text-cyan-900'>Sign up</button>
           </div>
 
           <div>{/* header */}
@@ -71,7 +71,7 @@ export default function App() {
                   </svg>
                 </div>
                   <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 16" strokeWidth={5.5} stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6"/></svg></button>
-                    <span className='text-sm font-medium uppercase'>score</span>
+                    <span className='text-sm font-medium uppercase '>score</span>
                   <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 18" strokeWidth={5.5} stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" /></svg></button>
                   <div className='text-sm font-medium uppercase'>Divario: ...</div>
                 </div>
@@ -134,7 +134,7 @@ export default function App() {
             </div>
 
             <div className='flex'>
-              <button className='bg-third text-white rounded-full py-3 px-6 pb-3 mt-10 mb-6 uppercase text-xs cursor-pointer tracking-wider font-bold border-primary md:border-2 animate-bounce'>Add Player</button>
+              <button className='bg-third text-white rounded-full py-3 px-6 pb-3 mt-10 mb-6 uppercase text-xs cursor-pointer tracking-wider font-bold border-primary md:border-2 animate-bounce hover:text-third hover:text-sm hover:bg-fourth hover:shadow-xl'>Add Player</button>
             </div>
 
             <div>{/* CARD */}
