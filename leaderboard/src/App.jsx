ec5ad5c06ab65cc5f0d750ea44f4423d7284824a
DianaBerte <dianaberte.go@gmail.com>
2023-10-12 11:41:38 +0200

    Fixing cards gap responsiveness

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -47,10 +47,10 @@ export default function App() {
           <div>{/* cards wrapper */}
             <h4 className='flex items-center justify-center text-4xl font-extrabold mt-8'>Winners</h4>
 
-            <div className='mt-8 pb-10 border-cyan-400 grid lg:grid-cols-3 gap-20 border-b'>
+            <div className='mt-8 pb-10 border-cyan-400 grid xl:grid-cols-3 lg:grid-cols-1 gap-20 md:gap-x-80 border-b'>
 
               {/* cards go here */}
-              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative w-72">{/* CARD 1*/}
+              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative lg:w-72 md:w-auto">{/* CARD 1*/}
                 <img className='w-full h-32 sm:h-48 object-cover rounded' src='https://res.cloudinary.com/degg5zebq/image/upload/v1696758941/Game%20characters/Game_character_4_tmvlol.png' alt='character img'></img>
                 <div className='m-4 text-secondary'>
                 <div className="flex items-center">
@@ -64,11 +64,11 @@ export default function App() {
                   <div>Divario: ...</div>
                 </div>
                 <div className='bg-white text-secondary text-6xl font-extrabold w-20 rounded-full p-3 absolute top-0 ml-3 mt-8 animate-bounce'>{/* badge position */}
-                  <div className='flex items-center justify-center'>2</div>
+                  <div className='flex items-center justify-center'>1</div>
                 </div>
               </div>
 
-              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative w-72">{/* CARD 2*/}
+              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative lg:w-72 md:w-auto">{/* CARD 2*/}
                 <img className='w-full h-32 sm:h-48 object-cover' src='https://res.cloudinary.com/degg5zebq/image/upload/v1696758942/Game%20characters/Game_character_2_bucm4k.png' alt='character img'></img>
                 <div className='m-4 text-secondary'>
                 <div className="flex items-center">
@@ -82,11 +82,11 @@ export default function App() {
                   <div>Divario: ...</div>
                 </div>
                 <div className='bg-white text-secondary text-6xl font-extrabold w-20 rounded-full p-3 absolute top-0 ml-3 mt-8 animate-bounce'>{/* badge position */}
-                  <div className='flex items-center justify-center'>1</div>
+                  <div className='flex items-center justify-center'>2</div>
                 </div>
               </div>
 
-              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative w-72">{/* CARD 3*/}
+              <div className="bg-white rounded overflow-hidden shadow-lg shadow-cyan-500 relative lg:w-72 md:w-auto">{/* CARD 3*/}
                 <img className='w-full h-32 sm:h-48 object-cover' src='https://res.cloudinary.com/degg5zebq/image/upload/v1696758942/Game%20characters/Game_character_1_snyslq.png' alt='character img'></img>
                 <div className='m-4 text-secondary'>
                 <div className="flex items-center">
