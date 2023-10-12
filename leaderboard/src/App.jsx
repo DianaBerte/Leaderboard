8086286f7959c47e15dc5d2e0b1ca9660aca2a2c
DianaBerte <dianaberte.go@gmail.com>
2023-10-12 16:16:31 +0200

    Update App.jsx

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -6,31 +6,36 @@ export default function App() {
     <div className="bg-cyan-900 min-h-screen text-primary">{/* body */}
 
       <div className='grid md:grid-cols-4'>{/* content wrapper */}
-        <div className='md:col-span-1'>
-          <nav>
+      <div className='md:col-span-1 md:flex md:justify-end'>
+          <nav className='text-right'>
             <div>
               <h1 className='text-2xl font-extrabold uppercase p-4 border-b border-cyan-700'>
                 <a className='hover:text-white' href="/">Leaderboard</a>
               </h1>
             </div>
-            <ul>
-              <li>
-                <button className='hover:text-lg hover:text-white'>
-                  <span>Home</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg></button>
-              </li>
-              <li>
-                <button className='hover:text-lg hover:text-white'>
-                  <span>About</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" /></svg>
+            <ul className='mt-6'>
+              <li className='py-1 flex justify-end'> {/* flex justify-end to align the content to the right */}
+                <button className='hover:text-lg hover:text-white px-4 flex justify-end'>
+                  <span>Home</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 ml-1"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
                 </button>
               </li>
-              <li>
-                <button className='hover:text-lg hover:text-white'>
-                  <span>Contact</span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
+              <li className='py-1 flex justify-end'>
+                <button className='hover:text-lg hover:text-white px-4 flex justify-end'>
+                  <span>About</span>
+                  <svg xmlns="http://w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" className="w-6 h-6 ml-1"><path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" /></svg>
+                </button>
+              </li>
+              <li className='py-1 flex justify-end'>
+                <button className='hover:text-lg hover:text-white px-4 flex justify-end'>
+                  <span>Contact</span>
+                  <svg xmlns="http://w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width={1.5} stroke="currentColor" className="w-6 h-6 ml-1">  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
                 </button>
               </li>
             </ul>
           </nav>
-        </div>{/* end navbar */}
+        </div>
+
 
         <div className='px-16 py-6 md:col-span-2'>{/* main content wrapper */}
           <div className='flex justify-center md:justify-end'>
