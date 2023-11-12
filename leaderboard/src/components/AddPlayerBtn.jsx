1febfed12cdee8184bc8810873c396a9edeff4d6
DianaBerte <dianaberte.go@gmail.com>
2023-11-12 17:05:06 +0100

    Form - customizable player

--- leaderboard/src/components/AddPlayerBtn.jsx
+++ leaderboard/src/components/AddPlayerBtn.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { ADD_NEW_PLAYER } from "../redux/actions";
+import { useState } from "react";
 
 // D9 - Redux Intro (remove from cart)
 // D11 - Multiple reducers & Async operations 3:06:00 (finished watching)
@@ -9,18 +10,50 @@ const AddPlayerBtn = (player) => {
     //the state my components are receiving is the whole state, not only one part of it.
     //So, I can travel through it by state.player.content, etc.
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const [showForm, setShowForm] = useState(false);
+    const [playerName, setPlayerName] = useState('');
+    const [playerScore, setPlayerScore] = useState('');
+
+    const handleAddPlayer = () => {
+        dispatch({
+            type: ADD_NEW_PLAYER,
+            payload: {name: playerName, score: playerScore}
+        });
+        setShowForm(false);
+        setPlayerName('');
+        setPlayerScore('');
+    }
 
     return(
         <>
         <div className='flex'>
-            <button onClick={() => {
-                dispatch({
-                    type: ADD_NEW_PLAYER,
-                    payload: player,
-                })
-            }} className='bg-third text-white rounded-full py-3 px-6 pb-3 mt-10 mb-6 uppercase text-xs cursor-pointer tracking-wider font-bold border-primary md:border-2 animate-bounce hover:text-third hover:text-sm hover:bg-fourth hover:shadow-xl'>Add Player</button>
-        </div>
+            <button onClick={() => setShowForm(true)}
+            className='bg-third text-white rounded-full py-3 px-6 pb-3 mt-10 mb-6 uppercase text-xs cursor-pointer tracking-wider font-bold border-primary md:border-2 animate-bounce hover:text-third hover:text-sm hover:bg-fourth hover:shadow-xl'
+            >Add Player</button>
+            </div>
+            
+            {showForm && (
+                <div className="form-container">
+                    <label>
+                        Player Name:
+                        <input
+                        type="text"
+                        value={playerName}
+                        onChange={(e) => setPlayerName(e.target.value)}
+                        ></input>
+                    </label>
+                    <label>
+                        Player Score:
+                        <input
+                        type="text"
+                        value={playerScore}
+                        onChange={(e) => setPlayerScore(e.target.value)}
+                        ></input>
+                    </label>
+                    <button onClick={handleAddPlayer}>Submit</button>
+                </div>
+           )}  
         </>
     );
 };
