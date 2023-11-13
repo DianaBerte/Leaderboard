fae9bdb3dd95422403789cb100b1158ced8775d8
DianaBerte <dianaberte.go@gmail.com>
2023-11-13 07:07:50 +0100

    Adding array of players

--- leaderboard/src/redux/reducers/playersReducer.js
+++ leaderboard/src/redux/reducers/playersReducer.js
@@ -1,8 +1,16 @@
 //import {SOMETHING} from actions
 
+//see: Array of Players
+
 const initialState = {
-    content: [],
-}
+    players: {
+        content: [
+            { id: 1, name: 'Gigi', score: 200 },
+            { id: 2, name: 'Boo', score: 150 },
+            { id: 3, name: 'Hash', score: 100 },
+        ],
+    },
+};
 
 const playersReducer = (state = initialState, action) => {
     switch (action.type) {
