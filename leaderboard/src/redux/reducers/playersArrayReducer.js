2254a474997fd3d2568e2f30de1e8a62e93bf783
DianaBerte <dianaberte.go@gmail.com>
2023-11-22 07:11:06 +0100

    Sorting by score: needs to include App and PlayerAdded together

--- leaderboard/src/redux/reducers/playersArrayReducer.js
+++ leaderboard/src/redux/reducers/playersArrayReducer.js
@@ -7,7 +7,7 @@ export const initialState = {
         content: [
             { id: 1, image: 'https://res.cloudinary.com/degg5zebq/image/upload/v1696758941/Game%20characters/Game_character_4_tmvlol.png', name: 'Gigi', score: 200 },
             { id: 2, image: 'https://res.cloudinary.com/degg5zebq/image/upload/v1696758942/Game%20characters/Game_character_2_bucm4k.png', name: 'Boo', score: 350 },
-            { id: 3, image: 'https://res.cloudinary.com/degg5zebq/image/upload/v1696758942/Game%20characters/Game_character_1_snyslq.png', name: 'Hash', score: 400 },
+            { id: 3, image: 'https://res.cloudinary.com/degg5zebq/image/upload/v1696758942/Game%20characters/Game_character_1_snyslq.png', name: 'Hash', score: 100 },
         ],
     },
 };
