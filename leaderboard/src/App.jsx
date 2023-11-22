2f2e63dc82bc64e0d6f0c4deeac48f434bf6ead4
DianaBerte <dianaberte.go@gmail.com>
2023-11-22 06:36:43 +0100

    Index rendered dinamically in PlayerAdded ✅

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -13,6 +13,8 @@ export default function App() {
   ? [...playersState.players.content].sort((a, b) => b.score - a.score)
   : [];
 
+  const fourthCardIndex = sortedPlayers.length + 1;
+
   return (
     <div className="bg-cyan-900 min-h-screen text-primary">{/* body */}
 
@@ -116,9 +118,9 @@ export default function App() {
 
             </div>
 
-            <div>{/* FOURTH CARD */}
+            {/* <div>
                 <div className='mb-6 py-4 px-4 text-white bg-secondary rounded overflow-hidden shadow-lg shadow-cyan-500'>
-                  <span className='text-2xl font-extrabold text-fourth rounded-full animate-pulse'>4</span>
+                  <span className='text-2xl font-extrabold text-fourth rounded-full animate-pulse'>{fourthCardIndex}</span>
                   <span className='text-2xl font-extrabold ml-4'>Random name</span>
                   <span className='text-xl font-bold animate-pulse text-fourth ml-4 mr-4'>20pt</span>
                   <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 16" strokeWidth={5.5} stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6"/></svg></button>
@@ -126,9 +128,9 @@ export default function App() {
                   <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 18" strokeWidth={5.5} stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m6-6H6" /></svg></button>
                   <span className='text-md font-medium uppercase ml-4'>Gap: ...</span>
                 </div>
-              </div>
+              </div> */}
 
-              <PlayerAdded />
+              <PlayerAdded sortedPlayers={sortedPlayers} />
 
           </div>{/* cards wrapper */}
 
--- leaderboard/src/components/PlayerAdded.jsx
+++ leaderboard/src/components/PlayerAdded.jsx
@@ -1,15 +1,15 @@
 import { useSelector } from "react-redux";
 
-const PlayerAdded = () => {
+const PlayerAdded = ({sortedPlayers}) => {
     //travel through state: state.player.content etc.
     const players = useSelector(state => state.singlePlayer.content);
 
     return(
         <>
         {players.map((player, index) => (
-                    <div key={index}>{/* CARD */}
+                    <div key={index}>
                     <div className='mb-6 py-4 px-4 text-white bg-secondary rounded overflow-hidden shadow-lg shadow-cyan-500'>
-                        <span className='text-2xl font-extrabold text-fourth rounded-full animate-pulse'>5</span>
+                        <span className='text-2xl font-extrabold text-fourth rounded-full animate-pulse'>{sortedPlayers.length + index + 1}</span>
                         <span className='text-2xl font-extrabold ml-4'>{player.name}</span>
                         <span className='text-xl font-bold animate-pulse text-fourth ml-4 mr-4'>{player.score}pt</span>
                         <button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 16" strokeWidth={5.5} stroke="currentColor" className="w-4 h-4"><path strokeLinecap="round" strokeLinejoin="round" d="M18 12H6"/></svg></button>
