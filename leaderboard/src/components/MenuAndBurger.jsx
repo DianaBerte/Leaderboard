c0cf46fbdf9005e232ef9a00ab7e15d0fb025691
DianaBerte <dianaberte.go@gmail.com>
2024-01-14 19:56:19 +0100

    cleans code

--- leaderboard/src/components/MenuAndBurger.jsx
+++ leaderboard/src/components/MenuAndBurger.jsx
@@ -20,8 +20,8 @@ const MenuAndBurger = () => {
           </nav>
 
             {isVisible && (
-            <ul className='mt-4 -mx-14 block absolute' id='menu'>{/* from md up: visible */}
-              <li className='py-1 flex justify-end'> {/* flex justify-end: content to the right */}
+            <ul className='mt-4 -mx-14 block absolute' id='menu'>
+              <li className='py-1 flex justify-end'> 
                 <button className='hover:scale-110 hover:text-white px-4 flex justify-end transition-all'>
                   <span>Home</span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-1"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
