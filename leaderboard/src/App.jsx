7d6ab9d11be059c75769e4795a334b95c22c0f9c
DianaBerte <dianaberte.go@gmail.com>
2024-01-14 19:32:23 +0100

    Update App.jsx

--- leaderboard/src/App.jsx
+++ leaderboard/src/App.jsx
@@ -140,6 +140,12 @@ export default function App() {
     }
   }, [playerIdToUpdate]);
 
+  const arrayOfLinks = [
+    {text: "Home",  href:"#", icon: "M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"},
+    {text: "About", href:'https://dianaberte.com/', icon: 'M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z'},
+    {text: "Contact", href: 'mailto:dianaberte.go@gmail.com', icon: 'M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75'},
+  ];
+
   return (
     <>
     {showParrot && (
@@ -161,12 +167,31 @@ export default function App() {
               <h1 className='text-2xl font-extrabold uppercase p-4 border-b border-cyan-700'>
                 <a className='hover:text-white' href="/">Leaderboard</a>
               </h1>
-
-              <MenuAndBurger />
-              
+              <MenuAndBurger /> 
             </div>
+
             <ul className='mt-6 hidden md:block' id='menu'>
-              <li className='py-1 flex justify-end'> 
+              {arrayOfLinks.map((link, index) => (
+                <li key={index} className={`py-1 flex justify-end ${link.text === 'About' ? 'about-link' : ''} `}>
+                  {link.href.startsWith('http') ? (
+                    <a href={link.href} className='hover:scale-110 hover:text-white px-4 flex justify-end transition-all' >
+                      <span>{link.text}</span>
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-1">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
+                      </svg>
+                    </a>
+                  ) : (
+                    <button className='hover:scale-110 hover:text-white px-4 flex justify-end transition-all'>
+                    <span>{link.text}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-1">
+                      <path strokeLinecap="round" strokeLinejoin="round" d={link.icon} />
+                    </svg>
+                  </button>
+                  )}
+                </li>
+              ))}
+              </ul>
+              {/* <li className='py-1 flex justify-end'> 
                 <button className='hover:scale-110 hover:text-white px-4 flex justify-end transition-all'>
                   <span>Home</span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-1"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>
@@ -183,8 +208,7 @@ export default function App() {
                   <span>Contact</span>
                   <svg xmlns="http://w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-1">  <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" /></svg>
                 </a>
-              </li>
-            </ul>                  
+              </li> */}
             <AddPlayerBtn handleAddPlayer={handleAddPlayer} className='justify-end' />
           </nav>
         </div>
